a25c16b349f4372327b3f28804257f9e3c0c5cca
satrop <stevenportas@me.com>
2023-02-21 15:52:18 -0800

    removing PWA

--- dev-dist/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/Designo/dev-sw.js?dev-sw', { scope: '/Designo/', type: 'classic' })
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [
-		react(),
-		VitePWA({
-			registerType: 'autoUpdate',
-			devOptions: {
-				enabled: true,
-			},
-		}),
-	],
+	plugins: [react()],
 	base: '/Designo/',
 	css: {
 		devSourcemap: true,
