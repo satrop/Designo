564fdf1a3f5967448d6943c60d3b2900d1fb3e1e
satrop <stevenportas@me.com>
2023-02-23 12:06:45 -0800

    Site done

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,5 +1,11 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+import illustrationUnitedKingdom from '/shared/desktop/illustration-united-kingdom.svg';
+import illustrationCanada from '/shared/desktop/illustration-canada.svg';
+import illustrationAustralia from '/shared/desktop/illustration-australia.svg';
 
 const Contact = () => {
 	// Class name to body tag for SCSS hooks
@@ -10,6 +16,24 @@ const Contact = () => {
 		};
 	}, []);
 
+	// Form
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const handleRegistration = (data) => console.log(data);
+	const handleError = (errors) => {};
+
+	let message = "Can't be empty";
+
+	const registerOptions = {
+		name: { required: `${message}` },
+		email: { required: `${message}` },
+		phone: { required: `${message}` },
+		message: { required: `${message}` },
+	};
+
 	return (
 		<HelmetProvider>
 			<Helmet>
@@ -18,7 +42,132 @@ const Contact = () => {
 				<meta name="description" content="Drop us a line." />
 			</Helmet>
 			<main id="main-content" className="wrapper flow">
-				<h1>Contact</h1>
+				<div
+					className="[ card contact-form-wrapper ] [ peach--bc white--fc radius--lg full-bleed ]"
+					data-background="pattern--welcome">
+					<div className="[ contact-message ] [ flow ]">
+						<h1>Contact Us</h1>
+						<p>
+							Ready to take it to the next level? Let's talk about
+							your project or idea and find out how we can help
+							your business grow. If you are looking for unique
+							digital experiences that's relatable to your users,
+							drop us a line.
+						</p>
+					</div>
+					<form
+						action="#"
+						className="[ form ] [ flow ]"
+						onSubmit={handleSubmit(
+							handleRegistration,
+							handleError
+						)}>
+						<div className="form-group">
+							<label className="visually-hidden">Name</label>
+							<input
+								name="name"
+								type="text"
+								placeholder="Name"
+								aria-invalid={errors.name ? 'true' : 'false'}
+								{...register('name', registerOptions.name)}
+							/>
+							<small className="text-danger">
+								{errors?.name && errors.name.message}
+							</small>
+						</div>
+						<div className="form-group">
+							<label className="visually-hidden">Email</label>
+							<input
+								type="email"
+								name="email"
+								placeholder="Email"
+								aria-invalid={errors.name ? 'true' : 'false'}
+								{...register('email', registerOptions.email)}
+							/>
+							<small className="text-danger">
+								{errors?.email && errors.email.message}
+							</small>
+						</div>
+						<div className="form-group">
+							<label className="visually-hidden">Phone</label>
+							<input
+								type="phone"
+								name="phone"
+								placeholder="Phone"
+								aria-invalid={errors.name ? 'true' : 'false'}
+								{...register('phone', registerOptions.phone)}
+							/>
+							<small className="text-danger">
+								{errors?.phone && errors.phone.message}
+							</small>
+						</div>
+						<div className="form-group">
+							<label className="visually-hidden">Message</label>
+							<input
+								type="textarea"
+								name="message"
+								placeholder="Message"
+								aria-invalid={errors.name ? 'true' : 'false'}
+								{...register(
+									'message',
+									registerOptions.message
+								)}
+							/>
+							<small className="text-danger">
+								{errors?.message && errors.message.message}
+							</small>
+						</div>
+						<button
+							aria-label="Submit"
+							className="[ button ] [ white--bc radius--sm ]">
+							Submit
+						</button>
+					</form>
+				</div>
+				<section className="[ card-info--wrapper ] [ flow ]">
+					<div className="[ card--info ] [ flow ]">
+						<div className="card--info__image">
+							<div className="circle" data-rotate="45deg"></div>
+							<img src={illustrationCanada} alt="" />
+						</div>
+						<div className="[ card--info__text ] [ flow ]">
+							<h3>Canada</h3>
+							<Link
+								to="/app-design"
+								className="[ button ] [ peach--bc radius--sm white--fc ]">
+								See location
+							</Link>
+						</div>
+					</div>
+					<div className="[ card--info ] [ flow ]">
+						<div className="card--info__image">
+							<div className="circle" data-rotate="45deg"></div>
+							<img src={illustrationAustralia} alt="" />
+						</div>
+						<div className="[ card--info__text ] [ flow ]">
+							<h3>AUSTRALIA</h3>
+							<Link
+								to="/app-design"
+								className="[ button ] [ peach--bc radius--sm white--fc ]">
+								See location
+							</Link>
+						</div>
+					</div>
+					<div className="[ card--info ] [ flow ]">
+						<div className="card--info__image">
+							<div className="circle" data-rotate="45deg"></div>
+							<img src={illustrationUnitedKingdom} alt="" />
+						</div>
+						<div className="[ card--info__text ] [ flow ]">
+							<h3>UNITED KINGDOM</h3>
+							<Link
+								to="/app-design"
+								className="[ button ] [ peach--bc radius--sm white--fc ]">
+								See location
+							</Link>
+						</div>
+					</div>
+				</section>
 			</main>
 		</HelmetProvider>
 	);
--- src/Pages/Locations.jsx
+++ src/Pages/Locations.jsx
@@ -1,5 +1,10 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { useEffect } from 'react';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import pin from '/shared/pin.svg';
 
 const Locations = () => {
 	// Class name to body tag for SCSS hooks
@@ -10,6 +15,25 @@ const Locations = () => {
 		};
 	}, []);
 
+	//Map Settings
+	const defaultZoom = [17];
+	const defaultScroll = false;
+
+	const canada = [43.64407062594163, -79.3945447653617];
+	const canadaCenter = [43.64407062594163, -79.3945447653617];
+
+	const australia = [-30.329213272592494, 149.78815406848258];
+	const australiaCenter = [-30.329213272592494, 149.78815406848258];
+	const australiaZoom = [13];
+
+	const england = [51.72794620179462, -3.8604607297099958];
+	const englandCenter = [51.72794620179462, -3.8604607297099958];
+
+	const mapPin = new L.Icon({
+		iconUrl: `${pin}`,
+		iconSize: [30, 30],
+	});
+
 	return (
 		<HelmetProvider>
 			<Helmet>
@@ -18,7 +42,108 @@ const Locations = () => {
 				<meta name="description" content="Come visit us!" />
 			</Helmet>
 			<main id="main-content" className="wrapper flow">
-				<h1>Locations</h1>
+				<div className="location">
+					<MapContainer
+						center={canadaCenter}
+						zoom={defaultZoom}
+						scrollWheelZoom={defaultScroll}
+						className="[ map ] [ radius--lg full-bleed ]">
+						<TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}@2x.png?api_key=aedb726c-c2ba-4d30-bc53-dfeac925f76b" />
+						<Marker position={canada} icon={mapPin}></Marker>
+					</MapContainer>
+					<div
+						className="[ card location__info ] [ radius--lg full-bleed peach--very-light--bc ]"
+						data-background="pattern--three">
+						<h2 className="peach--fc">Canada</h2>
+						<address className="flow">
+							<div className="col">
+								<strong>Designo Central Office</strong>
+								<br />
+								3886 Wellington Street
+								<br />
+								Toronto, Ontario M9C 3J5
+							</div>
+							<div className="col">
+								<strong>Contact</strong>
+								<br />
+								P : +1 253-863-8967
+								<br />M :{' '}
+								<a href="mailto:contact@designo.co">
+									{' '}
+									contact@designo.co
+								</a>
+							</div>
+						</address>
+					</div>
+				</div>
+				<div className="location flip">
+					<MapContainer
+						center={australiaCenter}
+						zoom={australiaZoom}
+						scrollWheelZoom={defaultScroll}
+						className="[ map ] [ radius--lg full-bleed ]">
+						<TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}@2x.png?api_key=aedb726c-c2ba-4d30-bc53-dfeac925f76b" />
+						<Marker position={australia} icon={mapPin}></Marker>
+					</MapContainer>
+					<div
+						className="[ card location__info ] [ radius--lg full-bleed peach--very-light--bc ]"
+						data-background="pattern--three">
+						<h2 className="peach--fc">Australia</h2>
+						<address className="flow">
+							<div className="col">
+								<strong>Designo AU Office</strong>
+								<br />
+								19 Balonne Street
+								<br />
+								New South Wales 2443
+							</div>
+							<div className="col">
+								<strong>Contact</strong>
+								<br />
+								P : (02) 6720 9092
+								<br />M :{' '}
+								<a href="mailto:contact@designo.au">
+									{' '}
+									contact@designo.au
+								</a>
+							</div>
+						</address>
+					</div>
+				</div>
+				<div className="location">
+					<MapContainer
+						center={englandCenter}
+						zoom={defaultZoom}
+						scrollWheelZoom={defaultScroll}
+						className="[ map ] [ radius--lg full-bleed ]">
+						<TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}@2x.png?api_key=aedb726c-c2ba-4d30-bc53-dfeac925f76b" />
+						<Marker position={england} icon={mapPin}></Marker>
+					</MapContainer>
+					<div
+						className="[ card location__info ] [ radius--lg full-bleed peach--very-light--bc ]"
+						data-background="pattern--three">
+						<h2 className="peach--fc">United Kingdom</h2>
+						<address className="flow">
+							<div className="col">
+								<strong>Designo UK Office</strong>
+								<br />
+								13 Colorado Way
+								<br />
+								Rhyd-y-fro SA8 9GA
+							</div>
+							<div className="col">
+								<strong>Contact</strong>
+								<br />
+								P : 078 3115 1400
+								<br />M :{' '}
+								<a href="mailto:contact@designo.uk">
+									{' '}
+									contact@designo.uk
+								</a>
+							</div>
+						</address>
+					</div>
+				</div>
 			</main>
 		</HelmetProvider>
 	);
